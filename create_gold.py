4b889055fd2b61f83b7c5d8e2104e7bd5f9652d7
Pathipan007 <kangzalnwza123@gmail.com>
2025-05-19 01:24:33 +0700

    add: add create sql gold file for evaluation

---
+++ create_gold.py
@@ -0,0 +1,14 @@
+import json
+
+# อ่าน dev_subset.json
+with open('bird/data/train/test_split_bird_20.json', 'r') as f:
+    data = json.load(f)
+
+# สร้าง dev_gold_subset.sql
+with open('bird/data/train/train_gold_test_split_20sql', 'w') as f:
+    for item in data:
+        sql = item['SQL'].strip()
+        db_id = item['db_id']
+        f.write(f"{sql}\t{db_id}\n")
+
+print("Created successful!!!")
